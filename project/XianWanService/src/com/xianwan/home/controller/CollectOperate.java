26748dd80d1914923e5ecd0e3a029d5d400dec0c
lvxueqi666 <1254343464@qq.com>
2019-12-05 10:41:19 +0800

    add

---
+++ project/XianWanService/src/com/xianwan/home/controller/CollectOperate.java
@@ -0,0 +1,70 @@
+package com.xianwan.home.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.xianwan.home.service.CollectService;
+
+import net.sf.json.JSONArray;
+
+/**
+ * Servlet implementation class CollectOperate
+ */
+@WebServlet("/CollectOperate")
+public class CollectOperate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CollectOperate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userAccount = request.getParameter("userAccount");
+		String commodityId = request.getParameter("commodityId");
+		String operate = request.getParameter("operate");
+		CollectService cs = new CollectService();
+		List<Integer> list = new ArrayList<>();
+		if(userAccount != null) {
+			if(operate.equals("query")) {
+				list = cs.queryCommodityIdByUserAccount(userAccount);
+				JSONArray jsonArray = JSONArray.fromObject( list );
+				System.out.println(jsonArray.size());
+				response.getWriter().print(jsonArray.toString());
+			}else if(operate.equals("add")){
+				cs.addCollection(Integer.parseInt(commodityId), userAccount);
+			}else if(operate.equals("cancel")){
+				cs.cancelCollection(Integer.parseInt(commodityId), userAccount);
+			}else {
+				if(cs.adjustIfExistCollection(Integer.parseInt(commodityId), userAccount)) {
+					response.getWriter().print("exist");
+				}else {
+					response.getWriter().print("unexist");
+				}
+			}
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ project/XianWanService/src/com/xianwan/home/dao/HomeDao.java
@@ -0,0 +1,89 @@
+package com.xianwan.home.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.xianwan.util.DBUtil;
+import com.xianwan.util.SnowflakeIdWorker;
+import com.xianwan.home.entity.Commodity;
+
+public class HomeDao {
+	
+	public List<Commodity> queryCommodity(String type) {
+		List<Commodity> coms = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		String sq = "select * from commodity as t1 join (select round(rand() * (select max(id) from commodity)) as id) as t2 where t1.id >= t2.id and attr = '" + type + "'order by t1.id limit 8";
+		//String sql = "select * from commodity where attr = '" + type + "' and id >= (select floor(RAND() * (select MAX(id) from commodity))) order by id limit 10";
+		conn = DBUtil.getConn();
+		try {
+			pstm = conn.prepareStatement(sq);
+			rs = pstm.executeQuery();
+			while(rs.next()) {
+				Commodity com = new Commodity();
+				com.setId(rs.getInt(1));
+				com.setImage(rs.getString(2));
+				com.setIntroduce(rs.getString(3));
+				com.setPrice(rs.getString(4));
+				com.setTag(rs.getString(5));
+				com.setUserAccount(rs.getString(6));
+				com.setIcon(rs.getString(7));
+				com.setUserName(rs.getString(8));
+				com.setAttr(rs.getString(9));
+				com.setShowLike(rs.getString(10));
+				coms.add(com);
+			}
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return coms;
+	}
+	
+	public void insertCommodity(String image,String introduce,String price,String tag,String userAccount) {
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		String sql = "insert into commodity values(?,?,?,?,?,?)";
+		conn = DBUtil.getConn();
+		SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0, 0);
+		long id = idWorker.nextId();
+		try {
+			pstm = conn.prepareStatement(sql);
+			pstm.setLong(1, id);
+			pstm.setString(2, image);
+			pstm.setString(3, introduce);
+			pstm.setString(4, price);
+			pstm.setString(5, tag);
+			pstm.setString(6, userAccount);
+			pstm.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
