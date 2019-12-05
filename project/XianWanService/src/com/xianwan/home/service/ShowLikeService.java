4ab94fc2a92748879d7e667a23b50cb951302993
lvxueqi666 <1254343464@qq.com>
2019-12-05 10:37:03 +0800

    rm

--- project/XianWanService/src/com/xianwan/home/service/ShowLikeService.java
+++
@@ -1,9 +0,0 @@
-package com.xianwan.home.service;
-
-import com.xianwan.home.dao.ShowLikeDao;
-
-public class ShowLikeService {
-	public void modifyLikeCount(String commodityId,String operate) {
-		new ShowLikeDao().modifyLikeCount(commodityId,operate);
-	}
-}
