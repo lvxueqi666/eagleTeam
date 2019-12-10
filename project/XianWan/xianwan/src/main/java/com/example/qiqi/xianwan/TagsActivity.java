73eb95b971746d83f06c8547b69df6b68f9de50a
李同 <1403062814@qq.com>
2019-12-10 14:43:40 +0800

    update

--- project/XianWan/xianwan/src/main/java/com/example/qiqi/xianwan/TagsActivity.java
+++ project/XianWan/xianwan/src/main/java/com/example/qiqi/xianwan/TagsActivity.java
@@ -1,7 +1,10 @@
 package com.example.qiqi.xianwan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TagsActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class TagsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tags);
+
+        Button btn_tags = findViewById(R.id.btn_tags);
+        btn_tags.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TagsActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
