d73278f2ae36254fc0a678a6ad23825dfcc4c875
huoxuerui <995775499@qq.com>
2019-12-12 10:18:52 +0800

    add

---
+++ project/XianWanService/src/com/xianwan/headpic/dao/HeadpicDao.java
@@ -0,0 +1,31 @@
+package com.xianwan.headpic.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.HEAD;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.xianwan.headpic.entity.Headpic;
+import com.xianwan.util.DBUtil;
+
+public class HeadpicDao {
+	private List<Headpic> list = new ArrayList<Headpic>();
+	public List<Headpic> querytoHeadpic() throws SQLException{
+		Connection con = DBUtil.getConn();
+		String sql = "select * from headpic";
+		PreparedStatement pstm = con.prepareStatement(sql);
+		ResultSet rs = pstm.executeQuery();
+		while(rs.next()){
+			Headpic headpic = new Headpic();
+			headpic.setUserId(rs.getString(3));
+			headpic.setAddress(rs.getString(2));
+			list.add(headpic);
+		}
+		return list;
+	}
+}
---
+++ project/XianWanService/src/com/xianwan/headpic/entity/Headpic.java
@@ -0,0 +1,27 @@
+package com.xianwan.headpic.entity;
+
+public class Headpic {
+    private String id;
+    private String address;
+    private String userId;
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+    
+}
