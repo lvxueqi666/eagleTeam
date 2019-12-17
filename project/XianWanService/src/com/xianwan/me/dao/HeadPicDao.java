3ab981465376c3b8e5297f9a84e874d917024e6a
lvxueqi666 <1254343464@qq.com>
2019-12-17 08:07:02 +0800

    update

--- project/XianWanService/src/com/xianwan/me/dao/HeadPicDao.java
+++ project/XianWanService/src/com/xianwan/me/dao/HeadPicDao.java
@@ -12,17 +12,17 @@ import com.xianwan.util.DBUtil;
 
 public class HeadPicDao {
 	
-	public String queryHeadPic(String userId) {
+	public String queryHeadPic(String userAccount) {
 		Connection conn = null;
 		PreparedStatement pstm = null;
 		ResultSet rs = null;
-		String sql = "select address from HeadPic where userId = ?";
+		String sql = "select address from HeadPic where userAccount = ?";
 		String address = null;
 		
 		try {
 			conn = DBUtil.getConn();
 			pstm = conn.prepareStatement(sql);
-			pstm.setString(1, userId);
+			pstm.setString(1, userAccount);
 			rs = pstm.executeQuery();
 			while(rs.next()) {
 				address = rs.getString(1);
@@ -39,23 +39,23 @@ public class HeadPicDao {
 	/*
 	 * 每个用户仅保留一张头像，之前的头像需要被删除
 	 */
-	public void saveHeadPic(String id,String address,String userId,Date uploadTime) {
+	public void saveHeadPic(String id,String address,String userAccount,Date uploadTime) {
 		Connection conn = null;
 		PreparedStatement pstm = null;
 		String sql = "insert headpic values(?,?,?,?)";
-		String sql1 = "delete from headpic where userId = ?";
+		String sql1 = "delete from headpic where userAccount = ?";
 		try {
 			conn = DBUtil.getConn();
 			//删除
 			pstm = conn.prepareStatement(sql1);
-			pstm.setString(1, userId);
+			pstm.setString(1, userAccount);
 			pstm.executeUpdate();
 			
 			//添加
 			pstm = conn.prepareStatement(sql);
 			pstm.setString(1, id);
 			pstm.setString(2, address);
-			pstm.setString(3,userId);
+			pstm.setString(3,userAccount);
 			pstm.setDate(4, uploadTime);
 			pstm.executeUpdate();
 		
